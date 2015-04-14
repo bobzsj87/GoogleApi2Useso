d1362484cdf8c9fa41d468849b364d7d17800f6e
Bob Zhou <bobzhou@Bobs-MacBook-Pro.local>
2015-04-14 14:04:29 +0800

    add idonethis

--- background.js
+++ background.js
@@ -1,18 +1,20 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {    
-    if (info.url.indexOf("https://fonts.googleapis.com/") !== -1){
-      return {redirectUrl: info.url.replace("fonts.googleapis.com", "d2tsbgstdoao7s.cloudfront.net")};
+    // if (info.url.indexOf("https://fonts.googleapis.com/") !== -1){
+    //   return {redirectUrl: info.url.replace("fonts.googleapis.com", "fontsgoogle.campbbs.com.global.ssl.fastly.net")};
+    // }
+    // if (info.url.indexOf("https://ajax.googleapis.com/") !== -1){
+    //   return {redirectUrl: info.url.replace("ajax.googleapis.com", "ajaxgoogle.campbbs.com.global.ssl.fastly.net")};
+    // }
+    if (info.url.indexOf("https://dot29u55fapha.cloudfront.net") !== -1){
+      return {redirectUrl: info.url.replace("dot29u55fapha.cloudfront.net", "dn-idonethismirror.qbox.me")};
     }
-    if (info.url.indexOf("https://ajax.googleapis.com/") !== -1){
-      return {redirectUrl: info.url.replace("ajax.googleapis.com", "dfk9v0sw9lp6g.cloudfront.net")};
-    }
-
     return {redirectUrl: info.url.replace("googleapis", "useso")};
   },
   // filters
   {
     urls: [
-       "*://fonts.googleapis.com/*", "*://ajax.googleapis.com/*"
+       "*://fonts.googleapis.com/*", "*://ajax.googleapis.com/*" , "*://dot29u55fapha.cloudfront.net/*"
     ],
     types: ["stylesheet", "script"]
   },
