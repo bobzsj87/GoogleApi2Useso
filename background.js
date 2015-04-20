1210194acbc6d4a7a754fc94721426e34e165209
Bob Zhou <bobzhou@Bobs-MacBook-Pro.local>
2015-04-20 10:19:21 +0800

    add googleapis

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {    
-    // if (info.url.indexOf("https://fonts.googleapis.com/") !== -1){
-    //   return {redirectUrl: info.url.replace("fonts.googleapis.com", "fontsgoogle.campbbs.com.global.ssl.fastly.net")};
-    // }
-    // if (info.url.indexOf("https://ajax.googleapis.com/") !== -1){
-    //   return {redirectUrl: info.url.replace("ajax.googleapis.com", "ajaxgoogle.campbbs.com.global.ssl.fastly.net")};
-    // }
+    if (info.url.indexOf("https://fonts.googleapis.com/") !== -1){
+      return {redirectUrl: info.url.replace("fonts.googleapis.com", "fonts.lug.ustc.edu.cn")};
+    }
+    if (info.url.indexOf("https://ajax.googleapis.com/") !== -1){
+      return {redirectUrl: info.url.replace("ajax.googleapis.com", "ajax.lug.ustc.edu.cn")};
+    }
     if (info.url.indexOf("https://dot29u55fapha.cloudfront.net") !== -1){
       return {redirectUrl: info.url.replace("dot29u55fapha.cloudfront.net", "dn-idonethismirror.qbox.me")};
     }
